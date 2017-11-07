5e228b93bb8ec54215955876e970611d3557c201
paolaandyco71 <paolaandyco71@gmail.com>
2017-11-06 22:07:09 -0300

    ingreso contraseña

---
+++ src/pruebajava2/Contraseñajava.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+import javax.swing.JOptionPane;
+/******************************************************************************
+ * *Paola Bravo Cardenas
+ * @author LAB03
+ *Fecha:06 de Noviembre 2017
+ *Programa:Prueba2Paola
+ * Responsabilidad:Creacion clase
+ * */
+ 
+ 
+  public class Contraseñajava{
+  /**
+    
+       /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       Scanner teclado= new Scanner(System);
+       
+       //Declaramos variables
+    int contreseña=0;
+    int pass=0;
+    int intentos=1;
+    int maximo=2;
+  
+    while((contraseña!=1919))&&((intentos<maximo));
+    {
+     System.out. println("Ingresa contraseña");
+     
+    contraseña=teclado.nextInter();
+    
+    System.out. println("introduzca pass");
+    pass =teclado.nextInt();
+    if(intentos==1);
+    {
+      System.out. println("acceso denegado");
+      intentos=intentos-maximos
+    
+      System.out. println("contraseña y pass correctos");
+   
+     }
+       
+            
+        
+    
+    
+}
